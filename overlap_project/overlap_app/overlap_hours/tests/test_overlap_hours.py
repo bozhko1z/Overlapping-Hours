7bcf87f28badf165763acd394a7824cbb58f91e5
maddrum <maddrum9@gmail.com>
2025-07-03 15:26:34 +0300

    temp commit

--- overlap_project/overlap_app/overlap_hours/tests/test_overlap_hours.py
+++ overlap_project/overlap_app/overlap_hours/tests/test_overlap_hours.py
@@ -1,4 +1,7 @@
+import datetime
 import unittest
+
+from overlap_hours.funcs import overlapping
 from overlap_project.overlap_app.overlap_hours import funcs as f
 
 
@@ -34,3 +37,45 @@ class TestOverlappingHours(unittest.TestCase):
             {"Day": "Friday", "Start": "15:00", "End": "19:00"},
         ]
         self.assertTrue(f.overlapping_time(data))
+
+    def test_mock(self):
+        data = [
+            {"Day": "Monday", "Start": "09:00", "End": "14:00"},
+        ]
+        self.assertTrue(overlapping(data))
+
+    def test_mock_1(self):
+        data = [
+            {"Day": "Monday", "Start": "09:00", "End": "14:00"},
+            {"Day": "Tuesday", "Start": "09:00", "End": "14:00"},
+        ]
+        self.assertTrue(overlapping(data))
+
+    def test_mock_2(self):
+        data = []
+        for day in [
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday",
+        ]:
+            for hour in range(0, 24):
+                for minute in range(0, 60, 2):
+                    data.append(
+                        {
+                            "Day": day,
+                            "Start": f"{hour}:{minute}",
+                            "End": f"{hour}:{minute + 1}",
+                        },
+                    )
+
+        time_start = datetime.datetime.now()
+        result = overlapping(data)
+        time_end = datetime.datetime.now()
+        self.assertTrue(result)
+
+        duration_ms = (time_end - time_start).total_seconds() * 1000
+        print(f"Time taken: {duration_ms:.2f} ms")
